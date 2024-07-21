a9e5c03cbd6bffe79fd171b617b928071a0f19a1
Frank <heguer76@gmail.com>
2024-07-21 12:21:54 -0600

    worked on this light house error: List items (<li>) are not contained within <ul>, <ol> or <menu> parent elements. this was done: The ul elements inside the dropdown menus are now conditionally rendered based on the state (showAboutDropdown and showServices). This ensures the structure is valid even if the dropdowns are initially hidden

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -25,11 +25,22 @@ export default function Navbar() {
       }
     }
     document.addEventListener('mouseover', handleOutsideClickOrHover)
+    document.addEventListener('click', handleOutsideClickOrHover)
 
     return () => {
       document.removeEventListener('mouseover', handleOutsideClickOrHover)
+      document.removeEventListener('click', handleOutsideClickOrHover)
     }
   }, [servicesDropdownRef, aboutDropdownRef])
+
+  const handleKeyDown = (event, toggleDropdown, dropdownState) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault()
+      toggleDropdown(!dropdownState)
+    } else if (event.key === 'Escape') {
+      toggleDropdown(false)
+    }
+  }
 
   return (
     <nav>
@@ -38,85 +49,210 @@ export default function Navbar() {
         role='navigation'
         aria-label='Main Navigation'>
         <li className='px-4'>
-          <Link href={'/'}>Home</Link>
+          <Link href='/'>Home</Link>
         </li>
 
         <li className='dropdown relative px-4' ref={aboutDropdownRef}>
           <button
             className='link'
             onClick={() => setShowAboutDropdown(!showAboutDropdown)}
+            onKeyDown={(e) =>
+              handleKeyDown(e, setShowAboutDropdown, showAboutDropdown)
+            }
             aria-expanded={showAboutDropdown}
-            aria-controls='about-menu'>
+            aria-controls='about-menu'
+            aria-haspopup='true'>
             About
           </button>
-          <ul
-            id='about-menu'
-            className={`dropdown-menu absolute bg-green top-[100%] left-[-5rem] py-[1rem] w-[15rem] rounded text-sm text-white transition-all duration-300 ease-in-out ${
-              showAboutDropdown
-                ? 'opacity-1 translate-y-0 pointer-events-auto'
-                : 'opacity-0 translate-y-[-20px] pointer-events-none'
-            }`}
-            style={{ boxShadow: '0 2px 5px 0 rgba(0,0,0, .1)' }}>
-            <li className='p-1'>
-              <Link
-                href={'/about'}
-                className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
-                About Us
-              </Link>
-            </li>
-            <li className='p-1'>
-              <Link
-                href={'/offers'}
-                className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
-                Offers
-              </Link>
-            </li>
-            <li className='p-1'>
-              <Link
-                href={'/blogs'}
-                className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
-                Blogs
-              </Link>
-            </li>
-          </ul>
+          {showAboutDropdown && (
+            <ul
+              id='about-menu'
+              className='dropdown-menu absolute bg-green top-[100%] left-[-5rem] py-[1rem] w-[15rem] rounded text-sm text-white transition-all duration-300 ease-in-out'
+              style={{ boxShadow: '0 2px 5px 0 rgba(0,0,0, .1)' }}>
+              <li className='p-1'>
+                <Link
+                  href='/about'
+                  className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+                  About Us
+                </Link>
+              </li>
+              <li className='p-1'>
+                <Link
+                  href='/offers'
+                  className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+                  Offers
+                </Link>
+              </li>
+              <li className='p-1'>
+                <Link
+                  href='/blogs'
+                  className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+                  Blogs
+                </Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li className='px-4'>
-          <Link href={'/gallery'}>Gallery</Link>
+          <Link href='/gallery'>Gallery</Link>
         </li>
         <li className='dropdown relative px-4' ref={servicesDropdownRef}>
           <button
             className='link'
             onClick={() => setShowServices(!showServices)}
+            onKeyDown={(e) => handleKeyDown(e, setShowServices, showServices)}
             aria-expanded={showServices}
-            aria-controls='services-menu'>
+            aria-controls='services-menu'
+            aria-haspopup='true'>
             Services
           </button>
-          <ul
-            id='services-menu'
-            className={`dropdown-menu absolute bg-green top-[100%] left-[-5rem] py-[1rem] w-[15rem] rounded text-sm text-white transition-all duration-300 ease-in-out ${
-              showServices
-                ? 'opacity-1 translate-y-0 pointer-events-auto'
-                : 'opacity-0 translate-y-[-20px] pointer-events-none'
-            }`}
-            style={{ boxShadow: '0 2px 5px 0 rgba(0,0,0, .1)' }}>
-            {serviceLinks.map(({ service, href }, i) => (
-              <li className='p-1' key={i}>
-                <Link
-                  href={href}
-                  className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
-                  {service}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {showServices && (
+            <ul
+              id='services-menu'
+              className='dropdown-menu absolute bg-green top-[100%] left-[-5rem] py-[1rem] w-[15rem] rounded text-sm text-white transition-all duration-300 ease-in-out'
+              style={{ boxShadow: '0 2px 5px 0 rgba(0,0,0, .1)' }}>
+              {serviceLinks.map(({ service, href }, i) => (
+                <li className='p-1' key={i}>
+                  <Link
+                    href={href}
+                    className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+                    {service}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </li>
         <li className='px-4'>
-          <Link href={'/contact'}>Contact</Link>
+          <Link href='/contact'>Contact</Link>
         </li>
         <li className='px-4'>
-          <GreenButton url={'/booking'} title={'Booking'} />
+          <GreenButton url='/booking' title='Booking' />
         </li>
       </ul>
     </nav>
   )
 }
+
+// import Link from 'next/link'
+// import { GreenButton } from '../util/Buttons'
+// import { useState, useRef, useEffect } from 'react'
+// import { serviceLinks } from '../../../data/serviceLinks'
+
+// export default function Navbar() {
+//   const [showServices, setShowServices] = useState(false)
+//   const [showAboutDropdown, setShowAboutDropdown] = useState(false)
+//   const servicesDropdownRef = useRef(null)
+//   const aboutDropdownRef = useRef(null)
+
+//   useEffect(() => {
+//     const handleOutsideClickOrHover = (event) => {
+//       if (
+//         servicesDropdownRef.current &&
+//         !servicesDropdownRef.current.contains(event.target)
+//       ) {
+//         setShowServices(false)
+//       }
+//       if (
+//         aboutDropdownRef.current &&
+//         !aboutDropdownRef.current.contains(event.target)
+//       ) {
+//         setShowAboutDropdown(false)
+//       }
+//     }
+//     document.addEventListener('mouseover', handleOutsideClickOrHover)
+
+//     return () => {
+//       document.removeEventListener('mouseover', handleOutsideClickOrHover)
+//     }
+//   }, [servicesDropdownRef, aboutDropdownRef])
+
+//   return (
+//     <nav>
+//       <ul
+//         className='hidden 2xl:flex items-center gap-4 text-green font-medium'
+//         role='navigation'
+//         aria-label='Main Navigation'>
+//         <li className='px-4'>
+//           <Link href={'/'}>Home</Link>
+//         </li>
+
+//         <li className='dropdown relative px-4' ref={aboutDropdownRef}>
+//           <button
+//             className='link'
+//             onClick={() => setShowAboutDropdown(!showAboutDropdown)}
+//             aria-expanded={showAboutDropdown}
+//             aria-controls='about-menu'>
+//             About
+//           </button>
+//           <ul
+//             id='about-menu'
+//             className={`dropdown-menu absolute bg-green top-[100%] left-[-5rem] py-[1rem] w-[15rem] rounded text-sm text-white transition-all duration-300 ease-in-out ${
+//               showAboutDropdown
+//                 ? 'opacity-1 translate-y-0 pointer-events-auto'
+//                 : 'opacity-0 translate-y-[-20px] pointer-events-none'
+//             }`}
+//             style={{ boxShadow: '0 2px 5px 0 rgba(0,0,0, .1)' }}>
+//             <li className='p-1'>
+//               <Link
+//                 href={'/about'}
+//                 className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+//                 About Us
+//               </Link>
+//             </li>
+//             <li className='p-1'>
+//               <Link
+//                 href={'/offers'}
+//                 className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+//                 Offers
+//               </Link>
+//             </li>
+//             <li className='p-1'>
+//               <Link
+//                 href={'/blogs'}
+//                 className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+//                 Blogs
+//               </Link>
+//             </li>
+//           </ul>
+//         </li>
+//         <li className='px-4'>
+//           <Link href={'/gallery'}>Gallery</Link>
+//         </li>
+//         <li className='dropdown relative px-4' ref={servicesDropdownRef}>
+//           <button
+//             className='link'
+//             onClick={() => setShowServices(!showServices)}
+//             aria-expanded={showServices}
+//             aria-controls='services-menu'>
+//             Services
+//           </button>
+//           <ul
+//             id='services-menu'
+//             className={`dropdown-menu absolute bg-green top-[100%] left-[-5rem] py-[1rem] w-[15rem] rounded text-sm text-white transition-all duration-300 ease-in-out ${
+//               showServices
+//                 ? 'opacity-1 translate-y-0 pointer-events-auto'
+//                 : 'opacity-0 translate-y-[-20px] pointer-events-none'
+//             }`}
+//             style={{ boxShadow: '0 2px 5px 0 rgba(0,0,0, .1)' }}>
+//             {serviceLinks.map(({ service, href }, i) => (
+//               <li className='p-1' key={i}>
+//                 <Link
+//                   href={href}
+//                   className='block w-full text-center py-2 rounded hover:bg-white hover:text-green capitalize'>
+//                   {service}
+//                 </Link>
+//               </li>
+//             ))}
+//           </ul>
+//         </li>
+//         <li className='px-4'>
+//           <Link href={'/contact'}>Contact</Link>
+//         </li>
+//         <li className='px-4'>
+//           <GreenButton url={'/booking'} title={'Booking'} />
+//         </li>
+//       </ul>
+//     </nav>
+//   )
+// }
